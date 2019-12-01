b3438a83a316ce3ad0ba1e60a6ab07c545d49420
Pontus Nagy <pontusnagy@gmail.com>
2019-12-01 10:39:27 +0100

    Day 1 Part 2 solved

--- src/1/2.hs
+++ src/1/2.hs
@@ -2,5 +2,14 @@
 
 main = print . solve . map read . lines =<< readFile "input.txt"
 
+fuelRequired :: Int -> Int
+fuelRequired n = floor (fromIntegral n / 3) - 2
+
+fuelRequired' :: Int -> Int -> Int
+fuelRequired' result x = if nextFuel > 0
+  then fuelRequired' (result + nextFuel) nextFuel
+  else result
+  where nextFuel = fuelRequired x
+
 solve :: [Int] -> Int
-solve = sum
+solve = sum . map (fuelRequired' 0)
